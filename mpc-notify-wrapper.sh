8f2fa9655dd2418e73a3b9266377dbbb813c5f30
Chad Sharp <crossroads1112@gmail.com>
2016-01-05 19:30:43 -0500

    Updated some stuff

--- mpc-notify-wrapper.sh
+++ mpc-notify-wrapper.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-icon=/usr/share/icons/Numix-Circle/scalable/apps/sonata.svg
+icon=/usr/share/icons/Numix-Square/scalable/apps/sonata.svg
 
-
-mpc $1 & &> /dev/null
+mpc $1 &> /dev/null
 case $1 in
     play|pause|next|prev|toggle)
         notification=$(cat <(mpc | sed '2!d' | awk '{print $1}') <(mpc | head -n1) | sed 'N;s/\n/ /') 
         notify-send -i $icon "mpc $1" "$notification"
         ;;
-    stop) notify-send -i $icon "mpc $1" "MPD stream stopped" ;;
+    stop) notify-send -i $icon "mpc $1" "MPD stream stopped" &> /dev/null;;
 esac
